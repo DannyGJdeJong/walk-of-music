9763d4f496e3e5e1e8e84f1173e72ce12fd32ced
DannyGJdeJong <dannygjdejong@gmail.com>
2019-10-24 14:54:03 +0200

    More sound testing

--- src/main.py
+++ src/main.py
@@ -7,11 +7,13 @@ GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 i = 0
 
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load("../audio/test.mp3")
+pygame.mixer.music.set_volume(100)
+
 while True:
     if GPIO.input(26):
-        import pygame
-        pygame.mixer.init()
-        pygame.mixer.music.load("../audio/test.mp3")
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy() == True:
             continue
