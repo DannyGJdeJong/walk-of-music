1745839419d6abaaa18f4d8f2fa3f13153789ed6
DannyGJdeJong <dannygjdejong@gmail.com>
2019-10-24 14:06:52 +0200

    dwadwafwafwaf

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ while True:
     if GPIO.input(26):
         import pygame
         pygame.mixer.init()
-        pygame.mixer.music.load("../audio/test.mpeg")
+        pygame.mixer.music.load("../audio/test.mp3")
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy() == True:
             continue
