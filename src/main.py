a0e5e9927c6eb0063295695bdc5261e19625775f
DannyGJdeJong <dannygjdejong@gmail.com>
2019-10-24 18:19:17 +0200

    Cleaned the program and added genres

--- src/main.py
+++ src/main.py
@@ -1,55 +1,63 @@
 import RPi.GPIO as GPIO
+import pygame
 import time
 import os
 
+"""
+pad 4 only works if 1,2,3 are validated
+pad 3 only works if 1,2 are valdated
+etc.
+
+when pad 4 is activated reset all pads
+
+every valid pad is activatable mutliple times
+"""
+
 STEP_PINS = [6, 13, 19, 26]
-GENRES = ["Rock"]
-GENRE_PINS = []
+GENRES = ["Jazz", "Rock", "Pop", "Disco", "Blues"]
+GENRE_PINS = [2, 3, 4, 17, 27]
+AUDIOPATH = "../audio"
+EXTENSION = ".ogg"
 
 GPIO.setmode(GPIO.BCM)
 
 for pin in STEP_PINS:
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-import pygame
+# Init pygame mixer for delay-less audio output
 pygame.mixer.pre_init(44100, -16, 2, 1024)
 pygame.init()
 pygame.mixer.quit()
 pygame.mixer.init(44100, -16, 2, 1024)
 
-s1 = pygame.mixer.Sound("../audio/test.ogg")
-s1.set_volume(100)
-s1 = pygame.mixer.Sound("../audio/test.ogg")
-s1.set_volume(100)
+# Load songs
+songs = [[]]
+for g in range(len(GENRES)):
+    ss = []
+    for i in range(len(STEP_PINS)):
+        s = pygame.mixer.Sound(os.path.join(AUDIOPATH, GENRES[g] + str(i) + EXTENSION))
+        s.set_volume(100)
+        ss.append(s)
+    songs.append(ss)
 
-audiopath = "../audio"
-
-songs = []
-for i in range(4):
-    s = pygame.mixer.Sound(os.path.join(audiopath, str(i) + ".ogg"))
-    s.set_volume(100)
-    songs.append(s)
 c = pygame.mixer.Channel(0)
 
+# Set variables to be used between loops
+genre = 0
+
+# Enter main loop
 while True:
-    # if GPIO.input(26):
-    #     c.play(s)
-    #     while c.get_busy() == True:
-    #         continue
+    for g in range(len(GENRE_PINS)):
+        if GPIO.input(GENRE_PINS[g]):
+            if genre != g:
+                # Reset
+                genre = g
+
     for i in range(len(STEP_PINS)):
         if GPIO.input(STEP_PINS[i]):
-            c.play(songs[i])
+            c.play(songs[genre][i])
             while c.get_busy() == True:
                 continue
 
-# pygame.mixer.pre_init(44100, -16, 1, 512)
-# pygame.mixer.music.load("../audio/test.ogg")
-# pygame.mixer.music.set_volume(100)
-
-# while True:
-#     if GPIO.input(26):
-#         pygame.mixer.music.play()
-#         while pygame.mixer.music.get_busy() == True:
-#             continue
-
+# Clean-up after running
 GPIO.cleanup() 
